befadfdfc3469055f6d53be61278364876a19a8d
abiona01 <quadratwemimo@gmail.com>
2021-07-07 17:37:49 +0100

    server.js

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const port = process.env.port || 5000;
-app.listen(port, () => console.log(`Server started on port ${5000}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
